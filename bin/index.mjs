1f16e7082c2a640817caf8526898b93b4941d6e2
Moneko <meow.moneko@gmail.com>
2025-02-24 08:21:42 +0000

    FIX: path

--- bin/index.mjs
+++ bin/index.mjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 ':'; // # comment; exec /usr/bin/env node --experimental-modules --experimental-json-modules "$0" "$@"
 
-import { eslint, stylelint } from '../lib';
+import { eslint, stylelint } from '../lib/index.mjs';
 
 Promise.all([eslint(), stylelint()]);
